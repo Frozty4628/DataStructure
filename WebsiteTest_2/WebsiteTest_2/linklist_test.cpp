de0c359d1f183af14b63a2a167260beba34d9aef
suzume <zengxiangruizzz@gmail.com>
2018-04-03 17:02:03 +0800

    上机实习2已完成 , 未发现错误.

--- WebsiteTest_2/WebsiteTest_2/linklist_test.cpp
+++ WebsiteTest_2/WebsiteTest_2/linklist_test.cpp
@@ -5,9 +5,72 @@
 using namespace std;
 
 int main() {
+	Lists a;
+	//输入一个输入结束标志
+	int end_tag;
+	cin >> end_tag;
+	//输入线性表A
+	a.Input_WithEndtag(end_tag);
+	cout << "A is created as:";
+	a.OutputList();
+	cout << endl;
+	//在2位置插入元素10
+	int position, value;
+	cin >> position >> value;
+	a.Insert(position, value);
+	cout << "After inserted A is";
+	a.OutputList();
+	cout << endl;
+	//删除值为10的元素
+	cin >> value;
+	a.RemoveNode_ValueMatched(value);
+	cout << "After deleted A is";
+	a.OutputList();
+	cout << endl;
+	//查找元素9
+	cin >> value;
+	int find_result;
+	find_result = a.FindNodePosition_ValueMatched(value);
+	if (find_result) {
+		cout << value << " is located at index of " << find_result << endl;
+	} else {
+		cout << value << " is not found" << endl;
+	}
+	//查找元素22
+	cin >> value;
+	find_result = a.FindNodePosition_ValueMatched(value);
+	if (find_result) {
+		cout << value << " is located at index of " << find_result << endl;
+	} else {
+		cout << value << " is not found" << endl;
+	}
+	//输入一个输入结束标志
+	Lists b;
+	cin >> end_tag;
+	//输入线性表B
+	b.Input_WithEndtag(end_tag);
+	cout << "B is created as:";
+	b.OutputList();
+	cout << endl;
+	//求交集
+	Lists intersection_list;
+	Intersection(a, b, intersection_list);
+	cout << "A cross B is";
+	intersection_list.OutputList();
+	cout << endl;
+	//求并集
+	Lists unionset_list;
+	UnionSet(a, b, unionset_list);
+	cout << "A union B is";
+	unionset_list.OutputList();
+	cout << endl;
+	//求排序后的并集
+	sort(unionset_list);
+	cout << "A union B in sequence is";
+	unionset_list.OutputList();
+	cout << endl;
 
-
-	system("pause");
+	//system("pause");
 	return 0;
 }
 
@@ -16,7 +79,7 @@ inline Node::Node(Node * p = nullptr) {
 	next = p;
 }
 
-inline Node::Node(const int datain, Node * p) {
+inline Node::Node(const int datain, Node * p = nullptr) {
 	data = datain;
 	next = p;
 }
@@ -24,17 +87,17 @@ inline Node::Node(const int datain, Node * p) {
 inline Node::~Node() {
 }
 
-//List类的函数实现
-inline List::List() {
+//Lists类的函数实现
+inline Lists::Lists() {
 	first = new Node();
 	current = first;
 	length = 0;
 }
 
-inline List::~List() {
+inline Lists::~Lists() {
 }
 
-inline void List::Input_WithEndtag(int endtag) {
+inline void Lists::Input_WithEndtag(int endtag) {
 	//使用后插法
 	int datain;
 	while (true) {
@@ -49,19 +112,21 @@ inline void List::Input_WithEndtag(int endtag) {
 	}
 }
 
-inline void List::Insert(int position, int datain) {
-	Node * p = first->next;
+inline void Lists::Insert(int position, int datain) {
+	Node * p = first;
 	Node * newnode = new Node(datain);
-	for (int i = 1; i <= position; i++) {
+	for (int i = 1; i < position; i++) {
 		p = p->next;
 	}
 	newnode->next = p->next;
 	p->next = newnode;
-	current = current->next;
+	if (newnode->next == nullptr) {
+		current = newnode;
+	}
 	length++;
 }
 
-inline void List::RemoveNode_ValueMatched(int dataremove) {
+inline void Lists::RemoveNode_ValueMatched(int dataremove) {
 	//搜索地址
 	Node * p = first;
 	int i = 1;
@@ -80,7 +145,7 @@ inline void List::RemoveNode_ValueMatched(int dataremove) {
 	}
 }
 
-inline int List::FindNodePosition_ValueMatched(int datafind) {
+inline int Lists::FindNodePosition_ValueMatched(int datafind) {
 	int i = 1;
 	Node * p = first->next;
 	while (i <= length) {
@@ -97,30 +162,19 @@ inline int List::FindNodePosition_ValueMatched(int datafind) {
 	}
 }
 
-void List::OutputList() {
+void Lists::OutputList() {
 	int i = 1;
 	Node * p = first->next;
 	while (i <= length) {
 		cout << " " << p->data;
+		p = p->next;
 		i++;
 	}
 }
 
 //交并集函数的实现
 
-inline void Intersection(List & a, List & b, List & answer) {
-	/*
-		//复制a到answer里面
-		answer.length = a.length;
-		int i = 1;
-		Node * p = a.first->next;
-		Node * q = answer.first->next;
-		while (i <= answer.length) {
-		q->data = p->data;
-		i++;
-		}
-
-	*/
+inline void Intersection(Lists & a, Lists & b, Lists & answer) {
 	//求交集
 	Node * p = a.first->next;
 	Node * q = b.first->next;
@@ -128,49 +182,66 @@ inline void Intersection(List & a, List & b, List & answer) {
 	int j = 1;
 	int position = 1;
 	while (i <= a.length) {
-		while (j <= b.length) {
-			if (p->data == q->data) {
-				answer.Insert(position, p->data);
-				position++;
-				j = 1;
-				break;
-			}
-			j++;
-			q = q->next;
+		if (b.FindNodePosition_ValueMatched(p->data)) {
+			answer.Insert(position, p->data);
+			position++;
 		}
 		i++;
 		p = p->next;
 	}
 }
 
-inline void UnionSet(List & a, List & b, List & answer) {
+inline void UnionSet(Lists & a, Lists & b, Lists & answer) {
 	//复制a到answer里面
-	answer.length = a.length;
 	int i = 1;
-	Node * p = a.first->next;
-	Node * q = answer.first->next;
-	while (i <= answer.length) {
-		q->data = p->data;
+	Node * p = a.first;
+	int position = 1;
+	while (i <= a.length) {
+		p = p->next;
+		answer.Insert(position, p->data);
+		position++;
 		i++;
 	}
+	//debug
+	//answer.OutputList();
 	//求并集
-	p = a.first->next;
-	q = b.first->next;
+	p = b.first;
 	i = 1;
-	int j = 1;
-	int position = answer.length + 1;
-	while (i <= a.length) {
-		while (j <= b.length) {
-			if (p->data != q->data) {
-				answer.Insert(position, p->data);
-				position++;
-				j = 1;
-				break;
-			}
-			j++;
-			q = q->next;
+	position = answer.length + 1;
+	while (i <= b.length) {
+		p = p->next;
+		if (a.FindNodePosition_ValueMatched(p->data) == 0) {
+			answer.Insert(position, p->data);
+			position++;
 		}
 		i++;
+	}
+}
+
+inline void swap_node(Node * a, Node * b) {
+	//交换两个结点
+	int tmp;
+	tmp = a->data;
+	a->data = b->data;
+	b->data = tmp;
+}
+
+inline void sort(Lists & list) {
+	//使用冒泡排序
+	//首先判断结点个数是否<=1
+	if (list.length <= 1) {
+		return;
+	}
+	//之后开始排序
+	Node * p = list.first->next;
+	for (int i = 1; i <= list.length; i++) {
+		Node * q = p->next;
+		for (int j = i + 1; j <= list.length; j++) {
+			if (p->data > q->data) {
+				swap_node(p, q);
+			}
+			q = q->next;
+		}
 		p = p->next;
 	}
 }
--- WebsiteTest_2/WebsiteTest_2/linklist_test.h
+++ WebsiteTest_2/WebsiteTest_2/linklist_test.h
@@ -4,15 +4,17 @@
 #include <algorithm>
 using namespace std;
 
-class List;
+class Lists;
 
 class Node {
 public:
-	friend List;
-	Node(Node * p = nullptr);
-	Node(const int datain, Node * p = nullptr);
-	friend void Intersection(List & a, List & b, List & answer);
-	friend void UnionSet(List & a, List & b, List & answer);
+	friend Lists;
+	Node(Node * p);
+	Node(const int datain, Node * p);
+	friend void Intersection(Lists & a, Lists & b, Lists & answer);
+	friend void UnionSet(Lists & a, Lists & b, Lists & answer);
+	friend void swap_node(Node * a, Node * b);
+	friend void sort(Lists & list);
 	~Node();
 
 private:
@@ -21,17 +23,19 @@ private:
 
 };
 
-class List {
+class Lists {
 public:
-	List();
-	~List();
+	Lists();
+	~Lists();
 	void Input_WithEndtag(int endtag = 0);
 	void Insert(int position, int datain);
 	void RemoveNode_ValueMatched(int dataremove);
 	int FindNodePosition_ValueMatched(int datafind);
 	void OutputList();
-	friend void Intersection(List & a, List & b, List & answer);
-	friend void UnionSet(List & a, List & b, List & answer);
+	friend void Intersection(Lists & a, Lists & b, Lists & answer);
+	friend void UnionSet(Lists & a, Lists & b, Lists & answer);
+	friend void swap_node(Node * a, Node * b);
+	friend void sort(Lists & list);
 
 private:
 	Node * first;
@@ -39,5 +43,7 @@ private:
 	int length;
 };
 
-void Intersection(List & a, List & b, List & answer);
-void UnionSet(List & a, List & b, List & answer);
+void Intersection(Lists & a, Lists & b, Lists & answer);
+void UnionSet(Lists & a, Lists & b, Lists & answer);
+void swap_node(Node * a, Node * b);
+void sort(Lists & list);
